060ad14a974a9b481522046574ba293f7c96d9cb
PushkarOnGithub <pushkarsaini000@gmail.com>
2022-11-07 17:38:02 +0530

    Done

--- GitBasics.py
+++ GitBasics.py
@@ -4,5 +4,5 @@
 # git checkout -b BranchName : create a new branch named BranchName and copy the master branch to it
 # git checkout master : to switch to the master branch also changes all the data as it in the master branch
 # git checkout BranchName : switch to a branch and have the files stored in the branch
-# git merge BranchName : merge the data on master branch to your branch
+# git merge BranchName : merge the data on the branch to your master branch
 
