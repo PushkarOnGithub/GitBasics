d3fb1f440c929681f3b6af5770500c3ff7202425
PushkarOnGithub <pushkarsaini000@gmail.com>
2022-11-07 17:30:12 +0530

    Done

--- GitBasics.py
+++ GitBasics.py
@@ -2,4 +2,7 @@
 # git commit -m "Message" : commit the changes to the local repository with the message provided
 # git status : gives the status of all the pending commits and changes in the files 
 # git checkout -b BranchName : create a new branch named BranchName and copy the master branch to it
-# git checkout master : to switch to the master branch
+# git checkout master : to switch to the master branch also changes all the data as it in the master branch
+# git checkout BranchName : switch to a branch and have the files stored in the branch
+# git merge BranchName : merge the data on master branch to your branch
+
