5c5d7eb354acb811ad7b3fd68538d4879b11cfc0
PushkarOnGithub <pushkarsaini000@gmail.com>
2022-11-07 11:29:14 +0530

    First commit on a new branc

--- GitBasics.py
+++ GitBasics.py
@@ -2,3 +2,4 @@
 # git commit -m "Message" : commit the changes to the local repository with the message provided
 # git status : gives the status of all the pending commits and changes in the files 
 # git checkout -b BranchName : create a new branch named BranchName and copy the master branch to it
+# git checkout master : to switch to the master branch
