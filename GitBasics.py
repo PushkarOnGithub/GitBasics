b5df291c822caed6e840df9563eee2be0ff36b72
PushkarOnGithub <pushkarsaini000@gmail.com>
2022-11-07 11:19:57 +0530

    First Change

---
+++ GitBasics.py
@@ -0,0 +1,3 @@
+# git add 'filename' : to add file to the git local repository
+# git commit -m "Message" : commit the changes to the local repository with the message provided
+# git status : gives the status of all the pending commits and changes in the files 
